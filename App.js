630cb49ac0fd0375144ec0fae56840a2043ff17d
Srujan Thotapally <sthotapa@terpmail.umd.edu>
2018-03-26 16:41:35 -0400

    starter code

---
+++ App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    Dimensions,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View
+} from 'react-native';
+
+import Camera from 'react-native-camera';
+class App extends Component {
+  componentDidMount(){
+          console.log("HERE");
+  }
+    scanBarcode(data) {
+        console.log("xxxx");
+    }
+    render() {
+        return (
+
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}
+          onBarCodeRead={this.scanBarcode.bind(this)}
+          barCodeTypes={[Camera.constants.BarCodeType.qr]}>
+        </Camera>
+
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    }
+});
+
+export default App;
